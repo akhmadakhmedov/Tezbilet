cc44adffab47a90e41e9b9637a0c42e1b0759ec4
akhmadakhmedov <ahmedov.thy@gmail.com>
2023-12-14 16:00:47 +0600

    Postgresql database set

--- tezbilet/settings.py
+++ tezbilet/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'tezbilet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tezbiletdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Realahmed+1',
+        'HOST': 'localhost'
     }
 }
 
