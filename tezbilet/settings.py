738708e0c0b03c4caca4bd3d0845192421895270
unknown <ahmedov.thy@gmail.com>
2023-12-20 09:56:21 +0600

    asdfggh

--- tezbilet/settings.py
+++ tezbilet/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'tezbilet.wsgi.application'
 # Database
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'tezbilet_prod',
-#        'USER': 'dbadmin',
-#        'PASSWORD': '12345',
-#        'HOST': 'localhost'
-#    }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tezbilet_prod',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
