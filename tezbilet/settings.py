dad95da2d2fc304f3630e877e023e154b131350f
unknown <ahmedov.thy@gmail.com>
2023-12-20 07:10:35 +0600

    aaa

--- tezbilet/settings.py
+++ tezbilet/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'tezbilet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tezbilet_prod',
         'USER': 'dbadmin',
         'PASSWORD': '12345',
